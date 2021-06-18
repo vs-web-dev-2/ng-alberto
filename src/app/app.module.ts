8f986be6e42800d23a6d89959346aa65169df2e4
Alberto Basalo <albertobasalo@hotmail.com>
2021-06-18 12:58:44 +0200

    ng component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PieComponent } from './pie/pie.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PieComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/pie/pie.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pie',
+  templateUrl: './pie.component.html',
+  styleUrls: ['./pie.component.css'],
+})
+export class PieComponent implements OnInit {
+  public autor: string = 'Sergio Riquelme';
+  // public year: string = '2021';
+  // public year: number = 2021;
+  public year: Date = new Date();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
