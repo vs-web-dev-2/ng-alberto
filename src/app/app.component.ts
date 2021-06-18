ac570148882fecfffeb1de20a7b0441ac876fd38
Alberto Basalo <albertobasalo@hotmail.com>
2021-06-18 12:05:12 +0200

    hola mundo ng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng-alberto';
+  public titulo = 'Mi súper mega aplicación';
 }
