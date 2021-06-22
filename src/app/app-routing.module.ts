9ef3f5ae065139ef056e37eaaef6d36100ea793a
Alberto Basalo <albertobasalo@hotmail.com>
2021-06-22 12:43:06 +0200

    agregar ruta tecleo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TecleoComponent } from './tecleo/tecleo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'tecleo',
+    component: TecleoComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
