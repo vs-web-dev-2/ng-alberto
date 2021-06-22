2058fae3af0f3706906db6f70482d0486965a5ae
Alberto Basalo <albertobasalo@hotmail.com>
2021-06-22 12:46:55 +0200

    ruta raiz enlazada a componente portal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PortalComponent } from './portal/portal.component';
 import { TecleoComponent } from './tecleo/tecleo.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: PortalComponent,
+  },
+
   {
     path: 'tecleo',
     component: TecleoComponent,
