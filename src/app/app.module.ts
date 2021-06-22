624a435e1abe3f5144849da32c3bf025a66cb480
Alberto Basalo <albertobasalo@hotmail.com>
2021-06-22 10:18:37 +0200

    crear module y component para tecleo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PieComponent } from './pie/pie.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
+import { PieComponent } from './pie/pie.component';
+import { TecleoModule } from './tecleo/tecleo.module';
 
 @NgModule({
   declarations: [AppComponent, PieComponent, CabeceraComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, TecleoModule],
   providers: [],
   bootstrap: [AppComponent],
 })
